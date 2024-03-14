87e39fa835cad7b4085d9335c195566e781e39cd
esemene <michaelrosemary04@gmail.com>
2024-03-14 09:33:08 +0000

    Revamped my code
    made everything work, because I AM BATMAN

--- to.js
+++ to.js
@@ -1,16 +1,36 @@
-let myinput = document.getElementById('myinput')
+let input = document.getElementById('input');
+let msg = document.getElementById('msg');
+let addTodo = document.getElementById('addTodo');
+let display = document.getElementById('display');
 
-
-myinput.addEventListener('click', function() {
-
-    if (myinput.value === '') {
-        
-
-    }else{
-        
-    }
-        
-
+addTodo.addEventListener('click', function () {
     
-})
+    let list;
+    if (input.value === '') {
+        msg.innerHTML = `Please add a 'todo'`;
+        input.style.border = 'solid 1px red';
+        setTimeout(() => {
+            msg.innerHTML = ``;
+        input.style.border = 'solid 1px red';
+        }, 3000);
+    }else{
+        input.style.border = 'none';
+
+        list = document.createElement('li');
+        console.log(list);
+        list.innerHTML = input.value;
+        display.appendChild(list);
+        input.value = '';
+    };
+
+    list.addEventListener('click', function () {
+        list.style.textDecoration = 'line-through';
+        list.style.color = 'green';
+    });
+
+    list.addEventListener('dblclick', function () {
+        list.style.display = 'none';
+    });
+});
+
 
